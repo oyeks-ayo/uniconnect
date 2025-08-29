e6aa5ce218a61e6d5429b6492dc7be5ef064c1fa
Oyeks_Ayo <adamsonia62.gmail.com>
2025-08-29 20:23:08 +0100

    database updated

--- pkg/config.py
+++ pkg/config.py
@@ -2,5 +2,6 @@ import os
 
 class Appconfig(object):
     SECRET_KEY = os.getenv("SECRET_KEY", 'fallback_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", 'mysql+mysqlconnector://root@localhost/seenITApp')
+    # SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", 'mysql+mysqlconnector://root@localhost/uniconnect')
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", 'Adamsonia:Adamsonia62@@localhost/uniconnect')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
