b006ef9cdf67d406465fb1bba6a1f5da75eceac3
Oyeks_Ayo <adamsonia62.gmail.com>
2025-08-30 12:45:18 +0100

    adjusted the app for deployment

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import os
 from pkg import app
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5070)
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5070)
 
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))  # Render sets PORT env variable
-#     app.run(host="0.0.0.0", port=port, debug=False)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT env variable
+    app.run(host="0.0.0.0", port=port, debug=False)
